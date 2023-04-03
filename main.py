7b85ad4337a4a972182dd988b046be9611af6a53
Alireza Izadi <alirezaizad@outlook.com>
2023-04-03 16:33:31 +0430

    Final added a way to record progress and finished the app

--- main.py
+++ main.py
@@ -4,30 +4,41 @@ import random
 BACKGROUND_COLOR = "#B1DDC6"
 TITLE_FONT = ("Ariel", 40, "italic")
 WORD_FONT = ("Ariel", 60, "bold")
-french_word = ""
+current_card = {}
+to_learn = {}
+
 #-------------------READING DATA FILE---------------------#
-file =pandas.read_csv("./data/french_words.csv")
-french_dict = {row["French"]:row["English"] for index, row in file.iterrows()}
-french_words = [key for key in french_dict.keys()]
-#---------------------RANDOM WORD------------------------#
-def random_word():
-    global french_word
-    french_word =random.choice(french_words)
-    return  french_word
+
+try:
+    data =pandas.read_csv("./data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv("./data/french_words.csv")
+    print(original_data)
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
+    
 #--------------------------Next CARD---------------------------#
 def next_card():
-    global flip_timer
+    global flip_timer, current_card
     window.after_cancel(flip_timer)
-    random_word()
+    current_card = random.choice(to_learn)
     canvas.itemconfig(card, image = card_front_img)
     canvas.itemconfig(title, text="French", fill="black")
-    canvas.itemconfig(word, text = f"{french_word}", fill="black")
+    canvas.itemconfig(word, text = current_card["French"], fill="black")
     flip_timer = window.after(3000, flip_card)
+#----------------------------STORE KNOWN CARDS-------------------#
+def is_known():
+    to_learn.remove(current_card)
+    print(len(to_learn))
+    data = pandas.DataFrame(to_learn)
+    data.to_csv("data/words_to_learn.csv")
+    next_card()
 #----------------------------FLIP CARD------------------------#
 def flip_card():
     canvas.itemconfig(card, image= card_back_img)
     canvas.itemconfig(title, text="English", fill="white")
-    canvas.itemconfig(word, text = f"{french_dict[french_word]}", fill="white")
+    canvas.itemconfig(word, text = current_card["English"], fill="white")
     
 #--------------------------------UI--------------------------------#
 window = Tk()
@@ -41,16 +52,16 @@ card_back_img = PhotoImage(file="./images/card_back.png")
 card =canvas.create_image(400 , 268, image=card_front_img)
 canvas.grid(column=1,row=0, columnspan=2, padx=15, pady=15)
 #Title Text
-title =canvas.create_text(400, 150, text="French", font=TITLE_FONT)
+title =canvas.create_text(400, 150, text="", font=TITLE_FONT)
 #Word Text
-word = canvas.create_text(400, 263, text=f"{random_word()}", font = WORD_FONT)
+word = canvas.create_text(400, 263, text="", font = WORD_FONT)
 #Wrong Button
 wrong_image = PhotoImage(file="./images/wrong.png")
 wrong_button = Button(image=wrong_image, command=next_card,highlightthickness=0)
 wrong_button.grid(column=1, row=1)
 #Right Button
 right_image = PhotoImage(file="./images/right.png")
-right_button = Button(image=right_image, command=next_card,highlightthickness=0)
+right_button = Button(image=right_image, command=is_known,highlightthickness=0)
 right_button.grid(column=2, row=1)
 next_card()
 window.mainloop()
